6db27a3443ae486dd6ce1c671106b7c0f4af1531
StevenG0h <atong321.as@gmail.com>
2022-07-21 07:53:06 +0700

    hehehey

--- application/views/Admin/TambahTestimoni.php
+++ application/views/Admin/TambahTestimoni.php
@@ -4,14 +4,14 @@
             <div class="row">
                 <h1 class="text-center">Tambah Testimoni</h1>
             </div>
-            <form class="row" action="" method="post">
+            <form class="row" action="<?= base_url("Testimoni/addTestimoniService") ?>" method="post" enctype="multipart/form-data">
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Komentar Konsumen</label>
-                    <textarea name="" id="" cols="30" rows="10" class="form-control"></textarea>
+                    <textarea name="komentar" id="" cols="30" rows="10" class="form-control"></textarea>
                 </div>
                 <div class="mb-3">
                     <label for="formFile" class="form-label">Upload Gambar</label>
-                    <input class="form-control" type="file" id="formFile">
+                    <input name="gambar" class="form-control" type="file" id="formFile">
                 </div>
                 <input type="submit" value="Simpan" class="btn btn-primary">
             </form>
