b67bd3fcd9190e77f91c1aecaa3f3d9169e7ad59
kintan anyndia <kintan20ti@mahasiswa.pcr.ac.id>
2022-07-21 08:07:06 +0700

    menambahkan insert testimoni

--- application/controllers/Testimoni.php
+++ application/controllers/Testimoni.php
@@ -1,29 +1,21 @@
-<?php 
-    class Testimoni extends CI_Controller{
-        function index(){
-            echo "hello world";
-        }
-        function addTestimoni(){
-            $this->load->view("HeaderFooter/header.php");
-            $this->load->view("Admin/TambahTestimoni.php");
-        }
-        function addTestimoniService(){
-            $config['uploadPath'] = 'gambar/';
-            $config['allowed_types'] = 'jpg|jpeg|png|webp|gif';
-            $config['max_size'] = '30000';
-            $config['file_name'] = $_FILES['gambar']['name'];
-            $this->load->library('upload',$config);
-            if($this->upload->do_upload('gambar')){ 
-                echo " hehe2";
-                $uploadData = $this->upload->data(); 
-                $filename = $uploadData['file_name'];
-                $data['response'] = 'successfully uploaded '.$filename;
-             }else{ 
-    
-                echo " hehe"; 
-             } 
-            echo $this->input->post("komentar");
+<?php
+class Testimoni extends CI_Controller
+{
+    function index()
+    {
+        echo "hello world";
+    }
+    function addTestimoni()
+    {
+        $this->load->view("HeaderFooter/header.php");
+        $this->load->view("Admin/TambahTestimoni.php");
+    }
+    function addTestimoniService()
+    {
+        $filename = $_FILES['gambar']['name'];
+        $tmp = $_FILES['gambar']['tmp_name'];
+        if(move_uploaded_file($tmp, 'Gambar/' . $filename)){
             
         }
     }
-?>
+}
